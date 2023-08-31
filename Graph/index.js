bbc5a448bdae8f6dbf6ba012c1b30c05d5f5c575
gtcelvam <selvasizzler@gmail.com>
2023-08-31 22:10:55 +0530

    Add and Remove methods for vertex and edge defined.

--- Graph/index.js
+++ Graph/index.js
@@ -8,6 +8,15 @@ class Graph {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = new Set();
   }
 
+  //Delete Vertex
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) return;
+    for (let adjacentVertex of this.adjacencyList[vertex]) {
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+
   //Add Line
   addEdge(vertex1, vertex2) {
     if (!this.adjacencyList[vertex1]) this.addVertex(vertex1);
@@ -15,6 +24,27 @@ class Graph {
 
     this.adjacencyList[vertex1].add(vertex2);
     this.adjacencyList[vertex2].add(vertex1);
+  }
+
+  //Delete Edge
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].delete(vertex1);
+    this.adjacencyList[vertex2].delete(vertex2);
+  }
+
+  //Check if edge is available in the vertex
+  hasEdge(vertex1, vertex2) {
+    return (
+      this.adjacencyList[vertex1].has(vertex2) &&
+      this.adjacencyList[vertex2].has(vertex1)
+    );
+  }
+
+  //Display Vertex
+  display() {
+    for (let vertex in this.adjacencyList) {
+      console.log(vertex + "->" + [...this.adjacencyList[vertex]]);
+    }
   }
 }
 
@@ -25,3 +55,6 @@ graph.addVertex("C");
 
 graph.addEdge("A", "B");
 graph.addEdge("B", "C");
+
+graph.display();
+console.log(graph.hasEdge("A", "C"));
